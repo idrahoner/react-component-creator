f0bbf0bbd95187edcdb511f47304a3543e161cce
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-24 18:39:37 +0300

    Add posible create components or pages from cli

--- index.js
+++ index.js
@@ -1,42 +1,81 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-const files = process.argv.slice(2);
+const COMPONENTS_TYPE = Object.freeze({
+  components: "components",
+  pages: "pages",
+});
 
-const createComponent = async (componentsList) => {
+const capitalizeName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+const normalizeNames = (namesList) => {
+  const normalized = namesList.map((name) => {
+    const trimmedName = name.trim();
+    const words = trimmedName.split("-");
+    return words.map(capitalizeName).join("");
+  });
+  return normalized;
+};
+
+const createFiles = async (folderName, fileName) => {
+  await fs.writeFile(
+    path.join(folderName, "index.js"),
+    `export { default } from './${fileName}';\n`
+  );
+  await fs.writeFile(
+    path.join(folderName, fileName + ".jsx"),
+    `export default function ${fileName}() {\n\treturn <h1>This is ${fileName}!</h1>;\n}`
+  );
+  await fs.writeFile(path.join(folderName, fileName + ".styled.jsx"), "");
+};
+
+const createFolder = async (componentType, componentName) => {
+  const componentFolderPath = path.join(
+    __dirname,
+    "src",
+    componentType,
+    componentName
+  );
+  await fs.mkdir(componentFolderPath, { recursive: true });
+  return componentFolderPath;
+};
+
+const create = async (componentType, componentsList) => {
+  if (!componentsList.length) {
+    console.log("You should pass component(s) name");
+    return;
+  }
+
+  const correctedNames = normalizeNames(componentsList);
+
+  return await Promise.all(
+    correctedNames.map(async (componentName) => {
+      const folderPath = await createFolder(componentType, componentName);
+      await createFiles(folderPath, componentName);
+      console.log(`${componentName} created successfully!`);
+    })
+  );
+};
+
+const createStructure = async (action, components) => {
   try {
-    if (!componentsList.length) {
-      console.log("You should pass component(s) name");
-      return;
+    switch (action) {
+      case "--component":
+      case "-c":
+        return await create(COMPONENTS_TYPE.components, components);
+      case "--page":
+      case "-p":
+        return await create(COMPONENTS_TYPE.pages, components);
+      default:
+        return console.log(
+          "Set correct action flag: --component (-c) or --page (-p)!"
+        );
     }
-
-    await Promise.all(
-      componentsList.map(async (componentName) => {
-        const componentFolderPath = path.join(
-          __dirname,
-          "src",
-          "comonents",
-          componentName
-        );
-        await fs.mkdir(componentFolderPath, { recursive: true });
-        await fs.writeFile(
-          path.join(componentFolderPath, "index.js"),
-          `export {default} from './${componentName}';\n`
-        );
-        await fs.writeFile(
-          path.join(componentFolderPath, componentName + ".jsx"),
-          `export default function ${componentName}() {\nreturn <h1>This is ${componentName}!</h1>;\n}`
-        );
-        await fs.writeFile(
-          path.join(componentFolderPath, componentName + ".styled.jsx"),
-          ""
-        );
-        console.log(`${componentName} created successfully!`);
-      })
-    );
   } catch (error) {
     console.log(error);
   }
 };
 
-createComponent(files);
+const [action, ...files] = process.argv.slice(2);
+
+createStructure(action, files);
